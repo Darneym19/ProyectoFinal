18681446e28bb2b0c9e2a9d31e8528f64cfafad2
Darney Maya <100388710+Darneym19@users.noreply.github.com>
2023-05-15 19:23:51 -0500

    Cambio LinearLayout

--- app/src/main/java/com/mobie/proyectofinal/MainActivity.java
+++ app/src/main/java/com/mobie/proyectofinal/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TimerTask inicio = new TimerTask() {
+        /*TimerTask inicio = new TimerTask() {
             @Override
             public void run() {
                 Intent intent = new Intent(MainActivity.this, Principal.class);
@@ -22,6 +22,6 @@ public class MainActivity extends Activity {
             }
         };
         Timer tiempo = new Timer();
-        tiempo.schedule(inicio, 7000);
+        tiempo.schedule(inicio, 7000);*/
     }
 }
