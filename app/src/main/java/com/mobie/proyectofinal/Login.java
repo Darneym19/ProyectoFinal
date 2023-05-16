e2d6c12cad36500692f3b351fc116603cee2c1b7
shulder2@gmail.com <shulder2@gmail.com>
2023-05-15 19:48:13 -0500

    Go to register view

--- app/src/main/java/com/mobie/proyectofinal/Login.java
+++ app/src/main/java/com/mobie/proyectofinal/Login.java
@@ -2,13 +2,30 @@ package com.mobie.proyectofinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+
+    Button registrarse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        registrarse = findViewById(R.id.bt_registro);
+        registrarse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                go_registro(view);
+            }
+        });
+    }
+
+    public void go_registro(View view){
+        Intent i = new Intent(Login.this, Registro.class);
+        startActivity(i);
     }
 }
